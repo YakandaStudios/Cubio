0800ae45c41602bd1813fe2f0578fd4219523580
Kef <kevin.gdl.131@gmail.com>
2017-08-31 09:56:08 -0500

    Revert "enhace: fix: DoubleJump working right but using hardcode trick in IDLE"
    This reverts commit 5905f7fbae329a5b183bb7090796d59d0d0561f2.

--- States/IdleState.gd
+++ States/IdleState.gd
@@ -4,36 +4,28 @@ extends "res://States/StateBase.gd"
 var character_node = null
 
 func enter(character_node):
-	print("Enter function Idle")
+#	print("Enter function Idle")
 	self.character_node = 	character_node
 #	character_node.move_and_slside(Vector3(0,0,0), Vector3(0,1,0))
 	
 func update(delta):
-	
 #Jump State
-	if character_node.touch_floor:
-		if Input.is_action_just_pressed("ui_jump"):
-			emit_signal("changing_state",  character_node.jump_state_path.new() ) 
-	
-	#Turning state
-		if Input.is_action_just_pressed("a"):
-			emit_signal("changing_state", character_node.turning_state_path.new() ) 
-		if Input.is_action_just_pressed("d"):
-			emit_signal("changing_state", character_node.turning_state_path.new()) 
-	
-	#Do moving again, call it itself
-		if Input.is_action_just_pressed("w"):
-			emit_signal("changing_state", character_node.moving_state_path.new() ) 
-			
-		if Input.is_action_just_pressed("s"):
-			emit_signal("changing_state", character_node.moving_state_path.new() ) 
-##########################
-#HardCode in order to make the function of the double Jump, need to change the way of detect the floor
-##########################
-	else:
-		if Input.is_action_just_pressed("ui_jump"):
-			emit_signal("changing_state",  character_node.double_jump_state_path.new() ) 
+	if Input.is_action_just_pressed("ui_jump"):
+		emit_signal("changing_state",  character_node.jump_state_path.new() ) 
+
+#Turning state
+	if Input.is_action_just_pressed("a"):
+		emit_signal("changing_state", character_node.turning_state_path.new() ) 
+	if Input.is_action_just_pressed("d"):
+		emit_signal("changing_state", character_node.turning_state_path.new()) 
+
+#Do moving again, call it itself
+	if Input.is_action_just_pressed("w"):
+		emit_signal("changing_state", character_node.moving_state_path.new() ) 
+		
+	if Input.is_action_just_pressed("s"):
+		emit_signal("changing_state", character_node.moving_state_path.new() ) 
 #	
 func exit():
-#	pass
-	print("Exit function Idle")
+	pass
+#	print("Exit function Idle")
--- States/JumpState.gd
+++ States/JumpState.gd
@@ -1,30 +1,22 @@
 extends "res://States/StateBase.gd"
 
 var character_node = null
-var t = Timer.new()
 
 func enter(character_node):
-	print("Enter function JUMP")
+#	print("Enter function JUMP")
 	self.character_node = character_node
 	character_node.jump()
-
-	t.set_wait_time(10)
-	t.set_one_shot(true)
-	self.add_child(t)
-	t.start()
-	yield(t, "timeout")
 	
 func update(delta):
-##########################
-#HardCode in order to make the function of the double Jump, need to change the way of detect the floor
-##########################
+
+#Touch floor
+	if (character_node.touch_floor):
+		emit_signal("changing_state", character_node.idle_state_path.new()) 
+		
 #DoubleJump State
 	if Input.is_action_just_pressed("ui_jump"):
 		emit_signal("changing_state",  character_node.double_jump_state_path.new() ) 
-#Touch floor
-	if (character_node.touch_floor == true):
-		emit_signal("changing_state", character_node.idle_state_path.new()) 
+
 func exit():
-#	pass
-	t.queue_free()
-	print("Exit function JUMP")
+	pass
+#	print("Exit function JUMP")
